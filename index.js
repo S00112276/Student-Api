d78ccc95ef6344abb448b2ac9f03c4596292839a
S00112276 <S00112276@mail.itsligo.ie>
2018-02-17 00:01:27 +0000

    Added a Procfile.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 // Set port on 3000
 //const port = process.env.PORT || 3000;
 // Heroku
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 // Database
 const config = require('./config/database');
 // Routes
