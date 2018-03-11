769c1d11d9b6849cfe635b41c86ebc582383a270
S00112276 <S00112276@mail.itsligo.ie>
2018-03-11 17:59:01 +0000

    delete an entry

--- api/routes/diaryRoutes.js
+++ api/routes/diaryRoutes.js
@@ -1,5 +1,5 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
 const DiaryEntry = require('../models/diaryModel');
 const Course = require('../models/courseModel');
 
@@ -19,7 +19,7 @@ router.post('/addtodiary', (req, res, next) => {
 
     // Save New Diary Entry & Check For Errors
     DiaryEntry.addEntry(newEntry, (err, entry) => {
-        if(err) {
+        if (err) {
             res.json({ success: false, msg: 'Failed to add entry: ' + err });
         } else {
             res.json({ success: true, msg: 'Entry added to diary!' });
@@ -32,38 +32,48 @@ router.get('/diaryentries', (req, res) => {
     DiaryEntry.find({}, function (err, entry) {
         if (err)
             res.send(err);
-        
+
         res.json(entry);
     });
 });
 
 // Retrieve an Entry
- router.get('/diaryentry', (req, res) => {
-    DiaryEntry.find({ _id: req.params.diaryId }, function(err, entry) {
-        if(err)
+router.get('/diaryentry', (req, res) => {
+    DiaryEntry.find({ _id: req.params.diaryId }, function (err, entry) {
+        if (err)
             res.send(err);
-        
+
         res.json(entry);
     });
-}); 
+});
 
 // Update an Entry
 router.put('/updateentry', (req, res) => {
-    DiaryEntry.findOneAndUpdate({_id: req.params.diaryId}, req.body,
-    { new: true }, function (err, entry) {
-        if(err)
-            res.send(err);
-        
-        res.json(entry);
-    })
+    DiaryEntry.findOneAndUpdate({ _id: req.params.diaryId }, req.body,
+        { new: true }, function (err, entry) {
+            if (err)
+                res.send(err);
+
+            res.json(entry);
+        })
 });
+
+// Delete an Entry
+router.delete('/removefromdiary', (req, res) => {
+    DiaryEntry.findOneAndRemove({ _id: req.params.diaryId }, function (err) {
+        if (err)
+            res.send(err);
+        else
+            res.json({ message: 'Entry Deleted' });
+    });
+})
 
 // Retrieve All Courses
 router.get('/courses', (req, res) => {
     Course.find({}, function (err, course) {
-        if(err)
+        if (err)
             res.send(err);
-        
+
         res.json(course);
     })
 });
@@ -71,19 +81,19 @@ router.get('/courses', (req, res) => {
 // Retrieve Student Groups
 router.get('/studentgroups', (req, res) => {
     Course.find({}, function (err, studentgroup) {
-        if(err)
+        if (err)
             res.send(err);
-        
+
         res.json(studentgroup);
     })
 });
 
 // Retrieve Student GRoup where Group Name = Soft Dev A2
 router.get('/studentgroup', (req, res) => {
-    Course.find({ "groups.name":"Soft Dev A2" }, function (err, studentgroup) {
-        if(err)
+    Course.find({ "groups.name": "Soft Dev A2" }, function (err, studentgroup) {
+        if (err)
             res.send(err);
-        
+
         res.json(studentgroup);
     })
 });
